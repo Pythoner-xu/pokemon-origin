fbfdacbf784c78bae072e3eccaf60d7bad1f0509
wskongdesheng <wskongdesheng@163.com>
2015-03-08 00:31:09 +0800

    da

--- src/app/scenes/MainScene.lua
+++ src/app/scenes/MainScene.lua
@@ -2,21 +2,41 @@
 local wakeUpLate = false;
 local mainLayer = nil;
 local uiLayer = nil;
+local chooseButton = {};
 local MainScene = class("MainScene", function()
     return display.newScene("MainScene")
 end)
 
 function MainScene:ctor()
-    cc.ui.UILabel.new({
-            UILabelType = 2, text = "Hello, World", size = 64})
-        :align(display.CENTER, display.cx, display.cy)
-        :addTo(self)
-    mainLayer = display.newColorLayer(cc.c4b(0, 255, 0, 255))
+	chooseButton.RADIO_BUTTON_IMAGES = {
+		on = "choose.png"
+	}
+    
+    mainLayer = display.newColorLayer(cc.c4b(255, 255, 255, 255))
     mainLayer:setContentSize(display.width, display.height)
    	mainLayer:addTo(self)
     uiLayer = display.newLayer()
     uiLayer:setContentSize(display.width, display.height)
     uiLayer:addTo(self)
+    cc.ui.UILabel.new({
+            UILabelType = 2, text = "叮铃铃，叮铃铃（闹钟响了）", size = 64, color = display.COLOR_BLACK})
+        :align(display.CENTER, display.cx, display.cy)
+        :addTo(self)
+    local chooseGroup = cc.ui.UICheckBoxButtonGroup.new(display.TOP_TO_BOTTOM)
+    chooseGroup:addButton(cc.ui.UICheckBoxButton.new(chooseButton.RADIO_BUTTON_IMAGES)
+    		:setButtonLabel(cc.ui.UILabel.new({text = "继续睡", color = display.COLOR_BLACK}))
+    		:setButtonLabelOffset(20, 0)
+    		:align(display.LEFT_CENTER))
+    chooseGroup:addButton(cc.ui.UICheckBoxButton.new(chooseButton.RADIO_BUTTON_IMAGES)
+    		:setButtonLabel(cc.ui.UILabel.new({text = "继续睡", color = display.COLOR_BLACK}))
+    		:setButtonLabelOffset(20, 0)
+    		:align(display.LEFT_CENTER))
+    chooseGroup:setButtonsLayoutMargin(10,10)
+    chooseGroup:onButtonSelectChanged(function(event)
+    		printf("Option %d selected, Option %d unseleceted", event.selected, event.last)
+    	end)
+    chooseGroup:align(display.CENTER, display.cx, display.cy)
+    chooseGroup:addTo(self)
 end
 
 function MainScene:onEnter()
