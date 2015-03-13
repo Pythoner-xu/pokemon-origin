aaad2e413a44929396ca14320de28ec173026a03
wskongdesheng <wskongdesheng@163.com>
2015-03-14 00:05:39 +0800

    update

--- src/app/scenes/MainScene.lua
+++ src/app/scenes/MainScene.lua
@@ -3,47 +3,33 @@ local wakeUpLate = false;
 local mainLayer = nil;
 local uiLayer = nil;
 local chooseButton = {};
+local tmx = nil;
 local MainScene = class("MainScene", function()
     return display.newScene("MainScene")
 end)
 
 function MainScene:ctor()
+    display.addSpriteFrames("sprites.plist", "sprites.png")
 	chooseButton.RADIO_BUTTON_IMAGES = {
 		on = "choose.png"
 	}
 	
-	local tmx = ccexp.TMXTiledMap:create('city1.tmx')
+	tmx = ccexp.TMXTiledMap:create('city1.tmx')
 	tmx:align(display.CENTER, display.cx, display.cy)
     
     mainLayer = display.newColorLayer(cc.c4b(255, 255, 255, 255))
     mainLayer:setContentSize(display.width, display.height)
    	mainLayer:addTo(self)
-    uiLayer = display.newLayer()
-    uiLayer:setContentSize(display.width, display.height)
-    uiLayer:addTo(self)
-    cc.ui.UILabel.new({
-            UILabelType = 2, text = "叮铃铃，叮铃铃（闹钟响了）", size = 64, color = display.COLOR_BLACK})
-        :align(display.CENTER, display.cx, display.cy)
-        :addTo(self)
-    local chooseGroup = cc.ui.UICheckBoxButtonGroup.new(display.TOP_TO_BOTTOM)
-    chooseGroup:addButton(cc.ui.UICheckBoxButton.new(chooseButton.RADIO_BUTTON_IMAGES)
-    		:setButtonLabel(cc.ui.UILabel.new({text = "继续睡", color = display.COLOR_BLACK}))
-    		:setButtonLabelOffset(20, 0)
-    		:align(display.LEFT_CENTER))
-    chooseGroup:addButton(cc.ui.UICheckBoxButton.new(chooseButton.RADIO_BUTTON_IMAGES)
-    		:setButtonLabel(cc.ui.UILabel.new({text = "继续睡", color = display.COLOR_BLACK}))
-    		:setButtonLabelOffset(20, 0)
-    		:align(display.LEFT_CENTER))
-    chooseGroup:setButtonsLayoutMargin(10,10)
-    chooseGroup:onButtonSelectChanged(function(event)
-    		printf("Option %d selected, Option %d unseleceted", event.selected, event.last)
-    	end)
-    chooseGroup:align(display.CENTER, display.cx, display.cy)
-    chooseGroup:addTo(self)
     tmx:addTo(mainLayer)
 end
 
 function MainScene:onEnter()
+    local layer = tmx:getLayer('ground')
+    local tile = layer:getTileAt(cc.p(5,5))
+    local frames = display.newFrames("flower%02d.png", 1, 4)
+    local animation = display.newAnimation(frames, 1 / 4) -- 0.5 秒播放 8 桢
+    display.setAnimationCache("flower", animation)
+    tile:playAnimationForever(display.getAnimationCache("flower"))
 end
 
 function MainScene:onExit()
