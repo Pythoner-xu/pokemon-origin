429d333b8b410151880ac03e7a999e455b872dac
clark <wskongdesheng@163.com>
2015-03-16 00:47:00 +0800

    update
    update

--- src/app/scenes/MainScene.lua
+++ src/app/scenes/MainScene.lua
@@ -4,19 +4,19 @@ local mainLayer = nil;
 local uiLayer = nil;
 local chooseButton = {};
 local tmx = nil;
+local player =nil;
 local MainScene = class("MainScene", function()
     return display.newScene("MainScene")
 end)
 
 function MainScene:ctor()
-	
-	tmx = ccexp.TMXTiledMap:create('city1.tmx')
+	tmx = cc.TMXTiledMap:create('city1.tmx')
 	tmx:align(display.CENTER, display.cx, display.cy)
-    
     mainLayer = display.newColorLayer(cc.c4b(255, 255, 255, 255))
     mainLayer:setContentSize(display.width, display.height)
    	mainLayer:addTo(self)
     tmx:addTo(mainLayer)
+    
 end
 
 function MainScene:onEnter()
@@ -24,7 +24,12 @@ function MainScene:onEnter()
     local s = layer:getLayerSize()
     printf(s.width)
     printf(s.height)
-    
+    local tamara = cc.Sprite:create("red.png")
+    local p = tamara:getPosition()
+    p = CC_POINT_POINTS_TO_PIXELS(p)
+    local newZ = -(p.y + 32) / 16
+    tamara:setVertexZ(newZ)
+
 end
 
 function MainScene:onExit()
