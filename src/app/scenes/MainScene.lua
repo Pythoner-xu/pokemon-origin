42a2dfd456437e4af8dcafcdcb0af13acf257c5a
clark <wskongdesheng@163.com>
2015-04-02 00:16:26 +0800

    update

--- src/app/scenes/MainScene.lua
+++ src/app/scenes/MainScene.lua
@@ -23,6 +23,7 @@ CC_POINT_POINTS_TO_PIXELS = function(points)
 end
 
 function MainScene:ctor()
+
 	tmx = cc.TMXTiledMap:create('city1.tmx')
 	tmx:align(display.CENTER, display.cx, display.cy)
     mainLayer = display.newColorLayer(cc.c4b(255, 255, 255, 255))
@@ -32,19 +33,41 @@ function MainScene:ctor()
 end
 
 function MainScene:onEnter()
+    display.addSpriteFrames("heroin.plist", "heroin.png")
     local layer = tmx:getLayer('ground')
     local s = layer:getLayerSize()
-    local tamara = cc.Sprite:create("red.png")
+    local tamara = display.newSprite("#Red1_01.png")
+    local images = {
+        normal = "btn.png",
+        pressed = "btn.png",
+        disabled = "btn.png"
+    }
+    local down_walk_frames = display.newFrames("Red1_%02d.png", 1, 4)
+    local down_walk = display.newAnimation(down_walk_frames, 0.5 / 4)
+    down_walk:setRestoreOriginalFrame(true)
+    display.setAnimationCache("down_walk", down_walk)
+
     
     tmx:addChild(tamara, table.getn(tmx:getChildren()))
     tamara:retain()
     tamara:setAnchorPoint(cc.p(0,0))
-    tamara:setPosition(layer:getTileAt(cc.p(6,5)):getPosition())
-    local move = cc.MoveBy:create(10,cc.p(100, 50))
-    local back = move:reverse()
-    local seq = cc.Sequence:create(move, back)    
-    tamara:runAction( cc.RepeatForever:create(seq))
-    tmx:reorderChild(tamara, 0);
+    tamara:setPosition(layer:getTileAt(cc.p(10,10)):getPosition())
+    local move = cc.MoveBy:create(2,cc.p(0, 48)) 
+    tmx:reorderChild(tamara, 1);
+    cc.ui.UIPushButton.new(images, {scale9 = true})
+        :onButtonClicked(function(event)
+            printf('click')
+        end)
+        :onButtonPressed(function(event)
+            printf('pressed')
+            tamara:playAnimationOnce(display.getAnimationCache("down_walk"))
+            tamara:moveBy(0.5,0 , -48)
+        end)
+        :onButtonRelease(function(event)
+            printf('released')
+        end)
+        :align(display.LEFT_BOTTOM, 12, 12)
+        :addTo(self)
 end
 
 function MainScene:onExit()
