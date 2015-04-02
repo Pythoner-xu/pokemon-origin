93f8a2fa056697828e08137dfd9cc87ecdb4109b
clark <wskongdesheng@163.com>
2015-04-02 23:49:58 +0800

    update
    update

---
+++ src/app/classes/player.lua
@@ -0,0 +1,40 @@
+local Player = class("Player", function ()
+    return display.newSprite("#Red1_01.png")
+end)
+
+function Player:ctor()
+    self:addStateMachine()
+    display.addSpriteFrames("heroin.plist", "heroin.png")
+end
+
+function Player:doEvent(event)
+    self.fsm:doEvent(event)
+end
+
+function Player:addStateMachine()
+    self.fsm = {}
+    cc.GameObject.extend(self.fsm):addComponent("components.behavior.StateMachine"):exportMethods()
+    self.fsm:setupState({
+        initial = "idle",
+
+        events = {
+            {name = "walk_down", from = {"idle"}, to = "walk_down"},
+            {name = "normal", from = {"walk_down"}, to = "idle"}
+        },
+
+        callbacks = {
+            onenteridle = function ()
+                self:stopAllActions()
+            end,
+            onenterwalk_down = function ()
+                local move = cc.MoveBy:create(0.5,cc.p(0, -48)) 
+                printf('walk_down')
+                self:moveBy(2,0,-48)
+                self:playAnimationForever(display.getAnimationCache("down_walk"))
+                self:runAction(cc.RepeatForever:create(move))
+            end
+        }
+    })
+end
+
+return Player
--- src/app/scenes/MainScene.lua
+++ src/app/scenes/MainScene.lua
@@ -1,4 +1,4 @@
-
+local Player = import("..classes.player")  
 local wakeUpLate = false;
 local mainLayer = nil;
 local uiLayer = nil;
@@ -8,19 +8,6 @@ local player =nil;
 local MainScene = class("MainScene", function()
     return display.newScene("MainScene")
 end)
-
-CC_CONTENT_SCALE_FACTOR = function()
-    return cc.Director:getInstance():getContentScaleFactor()
-end
-
-
-CC_POINT_PIXELS_TO_POINTS = function(pixels)
-    return cc.p(pixels.x/CC_CONTENT_SCALE_FACTOR(), pixels.y/CC_CONTENT_SCALE_FACTOR())
-end
-
-CC_POINT_POINTS_TO_PIXELS = function(points)
-    return cc.p(points.x*CC_CONTENT_SCALE_FACTOR(), points.y*CC_CONTENT_SCALE_FACTOR())
-end
 
 function MainScene:ctor()
 
@@ -36,35 +23,37 @@ function MainScene:onEnter()
     display.addSpriteFrames("heroin.plist", "heroin.png")
     local layer = tmx:getLayer('ground')
     local s = layer:getLayerSize()
-    local tamara = display.newSprite("#Red1_01.png")
+    local down_walk_frames = display.newFrames("Red1_%02d.png", 1, 4)
+    local down_walk = display.newAnimation(down_walk_frames, 0.5 / 4)
+    down_walk:setRestoreOriginalFrame(true)
+    display.setAnimationCache("down_walk", down_walk)
+    player = Player.new()
     local images = {
         normal = "btn.png",
         pressed = "btn.png",
         disabled = "btn.png"
     }
-    local down_walk_frames = display.newFrames("Red1_%02d.png", 1, 4)
-    local down_walk = display.newAnimation(down_walk_frames, 0.5 / 4)
-    down_walk:setRestoreOriginalFrame(true)
-    display.setAnimationCache("down_walk", down_walk)
+    
 
     
-    tmx:addChild(tamara, table.getn(tmx:getChildren()))
-    tamara:retain()
-    tamara:setAnchorPoint(cc.p(0,0))
-    tamara:setPosition(layer:getTileAt(cc.p(10,10)):getPosition())
-    local move = cc.MoveBy:create(2,cc.p(0, 48)) 
-    tmx:reorderChild(tamara, 1);
+    tmx:addChild(player, table.getn(tmx:getChildren()))
+    player:retain()
+    player:setAnchorPoint(cc.p(0,0))
+    player:setPosition(layer:getTileAt(cc.p(10,10)):getPosition())
+    tmx:reorderChild(player, 1);
     cc.ui.UIPushButton.new(images, {scale9 = true})
         :onButtonClicked(function(event)
             printf('click')
+            player:doEvent("walk_down")
         end)
         :onButtonPressed(function(event)
             printf('pressed')
-            tamara:playAnimationOnce(display.getAnimationCache("down_walk"))
-            tamara:moveBy(0.5,0 , -48)
+            
+            
         end)
         :onButtonRelease(function(event)
             printf('released')
+            player:doEvent("normal")
         end)
         :align(display.LEFT_BOTTOM, 12, 12)
         :addTo(self)
